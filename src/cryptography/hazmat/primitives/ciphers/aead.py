36f81a0f25ba7f4003d3b9103eddd9aee9f7dca1
J. Nick Koston <nick@koston.org>
2021-07-31 11:41:12 -0500

    tweaks

--- src/cryptography/hazmat/primitives/ciphers/aead.py
+++ src/cryptography/hazmat/primitives/ciphers/aead.py
@@ -13,6 +13,9 @@ from cryptography.hazmat.backends.openssl.backend import backend
 
 class ChaCha20Poly1305(object):
     _MAX_SIZE = 2 ** 32
+    _KEY_LEN = 32
+    _NONCE_LEN = 12
+    _TAG_LENGTH = 16
 
     def __init__(self, key: bytes):
         if not backend.aead_cipher_supported(self):
@@ -22,19 +25,17 @@ class ChaCha20Poly1305(object):
             )
         utils._check_byteslike("key", key)
 
-        if len(key) != 32:
+        if len(key) != self._KEY_LEN:
             raise ValueError("ChaCha20Poly1305 key must be 32 bytes.")
 
         self._cipher_name = aead._aead_cipher_name(self)
-        self._tag_length = 16
-        self._nonce_len = 12
         self._key = key
         self._decrypt_ctx = None
         self._encrypt_ctx = None
 
     @classmethod
     def generate_key(cls) -> bytes:
-        return os.urandom(32)
+        return os.urandom(ChaCha20Poly1305._KEY_LEN)
 
     def encrypt(
         self,
@@ -44,7 +45,11 @@ class ChaCha20Poly1305(object):
     ) -> bytes:
         if not self._encrypt_ctx:
             self._encrypt_ctx = aead._aead_setup_with_fixed_nonce_len(
-                backend, self._cipher_name, self._key, self._nonce_len, aead._ENCRYPT
+                backend,
+                self._cipher_name,
+                self._key,
+                self._NONCE_LEN,
+                aead._ENCRYPT,
             )
 
         if associated_data is None:
@@ -64,7 +69,7 @@ class ChaCha20Poly1305(object):
             nonce,
             data,
             associated_data,
-            self._tag_length,
+            self._TAG_LENGTH,
         )
 
     def decrypt(
@@ -75,7 +80,11 @@ class ChaCha20Poly1305(object):
     ) -> bytes:
         if not self._decrypt_ctx:
             self._decrypt_ctx = aead._aead_setup_with_fixed_nonce_len(
-                backend, self._cipher_name, self._key, self._nonce_len, aead._DECRYPT
+                backend,
+                self._cipher_name,
+                self._key,
+                self._NONCE_LEN,
+                aead._DECRYPT,
             )
 
         if associated_data is None:
@@ -88,7 +97,7 @@ class ChaCha20Poly1305(object):
             nonce,
             data,
             associated_data,
-            self._tag_length,
+            self._TAG_LENGTH,
         )
 
     def _check_params(
@@ -100,7 +109,7 @@ class ChaCha20Poly1305(object):
         utils._check_byteslike("nonce", nonce)
         utils._check_bytes("data", data)
         utils._check_bytes("associated_data", associated_data)
-        if len(nonce) != self._nonce_len:
+        if len(nonce) != self._NONCE_LEN:
             raise ValueError("Nonce must be 12 bytes")
 
 

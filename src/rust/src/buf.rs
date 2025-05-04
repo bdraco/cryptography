f28c57e718c73ff6c540e8187cd415ed8a0940e3
J. Nick Koston <nick@koston.org>
2025-05-04 16:43:45 -0500

    keep comments

--- src/rust/src/buf.rs
+++ src/rust/src/buf.rs
@@ -73,6 +73,14 @@ mod py311_impl {
             let buf = if len == 0 {
                 &[]
             } else {
+                // SAFETY: _extract_buffer_length ensures that we have a valid ptr
+                // and length (and we ensure we meet slice's requirements for
+                // 0-length slices above), we're keeping pyobj alive which ensures
+                // the buffer is valid. But! There is no actually guarantee
+                // against concurrent mutation. See
+                // https://alexgaynor.net/2022/oct/23/buffers-on-the-edge/
+                // for details. This is the same as our cffi status quo ante, so
+                // we're doing an unsound thing and living with it.                
                 unsafe { slice::from_raw_parts(bufobj.buf_ptr() as *const u8, len) }
             };
 
@@ -103,6 +111,14 @@ mod py311_impl {
             let buf = if len == 0 {
                 &mut []
             } else {
+                // SAFETY: _extract_buffer_length ensures that we have a valid ptr
+                // and length (and we ensure we meet slice's requirements for
+                // 0-length slices above), we're keeping pyobj alive which ensures
+                // the buffer is valid. But! There is no actually guarantee
+                // against concurrent mutation. See
+                // https://alexgaynor.net/2022/oct/23/buffers-on-the-edge/
+                // for details. This is the same as our cffi status quo ante, so
+                // we're doing an unsound thing and living with it.                
                 unsafe { slice::from_raw_parts_mut(bufobj.buf_ptr() as *mut u8, len) }
             };
 

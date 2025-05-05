89e68cc67b6ef95f5d599dd0444cc93619379af9
J. Nick Koston <nick@koston.org>
2025-05-05 17:41:29 -0500

    wip

---
+++ packaging/pep517_backend/wrapper.py
@@ -0,0 +1,11 @@
+import sys
+from maturin import *  # Import everything from maturin
+
+MAX_MINOR_VERSION = 13  # Python 3.13 is latest supported version
+MAJOR = sys.version_info.major
+MINOR = max(sys.version_info.minor, MAX_MINOR_VERSION)
+
+os.environ["MATURIN_PEP517_ARGS"] = (
+    os.environ.get("MATURIN_PEP517_ARGS", "")
+    + f" --features=pyo3/abi3-py{MAJOR}{MINOR}"
+)

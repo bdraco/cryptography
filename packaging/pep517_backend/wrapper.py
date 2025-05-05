1daf9cff356faa7ef5efba36bd43475ce656ceb3
J. Nick Koston <nick@koston.org>
2025-05-05 18:05:26 -0500

    tweaks

--- packaging/pep517_backend/wrapper.py
+++ packaging/pep517_backend/wrapper.py
@@ -7,5 +7,9 @@ MINOR = min(sys.version_info.minor, MAX_MINOR_VERSION)
 
 current_args = os.environ.get("MATURIN_PEP517_ARGS", "")
 if "--features" not in current_args:
+    # This is a thin wrapper around maturin to set the ABI3 feature flag
+    # for the current Python version so we use the limited ABI3 for the
+    # current Python version as maturin does not have a way to do
+    # this automatically.
     features_arg = f" --features=pyo3/abi3-py{MAJOR}{MINOR}"
     os.environ["MATURIN_PEP517_ARGS"] = current_args + features_arg
